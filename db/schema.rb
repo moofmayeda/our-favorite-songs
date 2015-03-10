f9c9b465f7cb1341ee170a381865b235bca9281e
moof mayeda <moofmayeda@yahoo.com>
2015-03-10 16:23:19 -0700

    create artist model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150310225257) do
+ActiveRecord::Schema.define(:version => 20150310231116) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -46,10 +46,17 @@ ActiveRecord::Schema.define(:version => 20150310225257) do
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
 
+  create_table "artists", :force => true do |t|
+    t.text     "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
   create_table "songs", :force => true do |t|
     t.text     "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "artist_id"
   end
 
   create_table "users", :force => true do |t|
