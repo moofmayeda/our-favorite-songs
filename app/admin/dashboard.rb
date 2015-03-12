998ede3bce9f1cfd82e6925be9dd1cc07d8070df
moof mayeda <moofmayeda@yahoo.com>
2015-03-12 13:48:38 -0700

    display last 10 favorites on dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -16,7 +16,7 @@ ActiveAdmin.register_page "Dashboard" do
       column do
         panel "Recent Favorites" do
           ul do
-            Favorite.last(5).map do |favorite|
+            Favorite.recent.map do |favorite|
               li link_to(favorite.user.email, admin_user_path(favorite.user)) + " favorited " + link_to(favorite.song.name, admin_song_path(favorite.song))
             end
           end
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,5 @@
 class Favorite < ActiveRecord::Base
 	belongs_to :song
 	belongs_to :user
+	scope :recent, order("created_at DESC").limit(10)
 end
