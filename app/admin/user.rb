103431bb5b76b6bbd91f759f43ff9785a801834a
moof mayeda <moofmayeda@yahoo.com>
2015-03-12 14:21:19 -0700

    limit columns shown on users page

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,3 +1,10 @@
 ActiveAdmin.register User do
-
+	index do
+		column :id
+		column :username, sortable: :username do |user|
+			link_to(user.username, admin_user_path(user))
+		end
+		column :email
+		column :last_sign_in_at
+	end
 end
