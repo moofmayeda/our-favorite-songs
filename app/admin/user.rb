682adc29eb7c9fe86c1dee14fc1d9f74bcc6a3e2
moof mayeda <moofmayeda@yahoo.com>
2015-03-13 13:42:26 -0700

    create a new user through the admin interface

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -7,4 +7,14 @@ ActiveAdmin.register User do
 		column :email
 		column :last_sign_in_at
 	end
+
+	form do |f|
+    f.inputs "User Details" do
+      f.input :username
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
+  end
 end
