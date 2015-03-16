efa951da5fb1bc7a058170cc09effd1056c171f5
moof mayeda <moofmayeda@yahoo.com>
2015-03-16 15:08:50 -0700

    create song index page that displays all songs across all artists

--- app/admin/song.rb
+++ app/admin/song.rb
@@ -1,3 +1,14 @@
 ActiveAdmin.register Song do
-	belongs_to :artist
+	belongs_to :artist, optional: true
+	index do
+		column :artist, sortable: 'artists.name'
+		column :name
+		default_actions
+	end
+
+	controller do
+	  def scoped_collection
+	    end_of_association_chain.includes(:artist)
+	  end
+	end
 end
