3ffaee878a153f5a4acc5a31269090d0bfb79185
moof mayeda <moofmayeda@yahoo.com>
2015-03-18 15:00:42 -0700

    return a token from Stripe to use for a payment

--- app/assets/javascripts/customstripe.js
+++ app/assets/javascripts/customstripe.js
@@ -1,4 +1,24 @@
 $(document).ready(function() {
 	var key = $("#STRIPE_KEY").text();
 	Stripe.setPublishableKey(key);
+
+	$('#new_charge').submit(function(event) {
+		var $form = $(this);
+		$('input[type=submit]').attr('disabled', true);
+
+		Stripe.card.createToken($form, stripeResponseHandler);
+		return false;
+	});
 });
+
+function stripeResponseHandler(status, response) {
+	var $form = $('#new_charge');
+
+	if (response.error) {
+		console.log(response.error.message);
+	} else {
+		var token = response.id;
+		$form.append($('<input type="hidden" name="stripeToken" />').val(token));
+		$form.get(0).submit();
+	}
+}
